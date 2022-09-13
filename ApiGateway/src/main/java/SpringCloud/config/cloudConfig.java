0bd423d250d7e3da62127f6677bc7e4d650d6630
thexperias@gmail.com <thexperias@gmail.com>
2022-09-13 11:54:17 +0700

    Sukses setup Cloud gatewayRoutes

---
+++ ApiGateway/src/main/java/SpringCloud/config/cloudConfig.java
@@ -0,0 +1,19 @@
+package SpringCloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class cloudConfig {
+	@Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/car/**")
+                        .uri("http://localhost:8082/"))
+                .route(r -> r.path("/api/v1/customer/**")
+                        .uri("http://localhost:8081/"))
+                .build();
+    }
+}
