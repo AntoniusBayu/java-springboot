7490ae4c5aa6c9a01b149a12fb65b273beb28f55
thexperias@gmail.com <thexperias@gmail.com>
2022-09-13 10:50:53 +0700

    Init Api Gateway

---
+++ ApiGateway/src/test/java/SpringCloud/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringCloud.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
